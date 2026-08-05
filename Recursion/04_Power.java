efb58bf69bd000316ee610f98d34c360d3e2b2be
kumarr-shubham <kumarshubham5646@gmail.com>
2026-08-05 18:47:44 +0530

    Update and rename Power.java to 04_Power.java

--- Recursion/04_Power.java
+++ Recursion/04_Power.java
@@ -11,13 +11,13 @@ public class Power {
         int b = sc.nextInt();
         System.out.println(a+" raised to the power "+b+" is "+powLog(a,b));
     }
-    public static int powLog(int a, int b){
+    public static int powLog(int a, int b){ //2
         if(b==0) return 1;
         int call = powLog(a,b/2);
         if(b%2 == 0) return call*call;
         else return a*call*call;
     }
-    public static int pow(int a, int b){
+    public static int pow(int a, int b){ //1
         if(b==0) return 1;
         return a*pow(a,b-1);
     }
