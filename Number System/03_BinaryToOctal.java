d40cc25f0a34b1fdbcb4a94a2d4e481c0aacbfa9
kumarr-shubham <kumarshubham5646@gmail.com>
2026-08-06 12:49:52 +0530

    Create 03_BinaryToOctal.java

---
+++ Number System/03_BinaryToOctal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class BinaryToOctal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int binary = sc.nextInt();
+        int decimal = 0;
+        int base = 1;
+        while(binary > 0){
+            int digit = binary % 10;
+            decimal += digit * base;
+            base *= 2;
+            binary /= 10;
+        }
+        int octal = 0;
+        base = 1;
+        while(decimal > 0){
+            int rem = decimal % 8;
+            octal += rem * base;
+            base *= 10;
+            decimal /= 8;
+        }
+        System.out.println(octal);
+    }
+}
