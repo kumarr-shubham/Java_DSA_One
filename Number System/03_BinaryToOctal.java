e71e7f08bfa873066b80d01fbc0ee55467f97a76
kumarr-shubham <kumarshubham5646@gmail.com>
2026-08-06 13:07:23 +0530

    Update 03_BinaryToOctal.java

--- Number System/03_BinaryToOctal.java
+++ Number System/03_BinaryToOctal.java
@@ -3,6 +3,7 @@ public class BinaryToOctal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int binary = sc.nextInt();
+        //Binary → Decimal
         int decimal = 0;
         int base = 1;
         while(binary > 0){
@@ -11,6 +12,7 @@ public class BinaryToOctal {
             base *= 2;
             binary /= 10;
         }
+        //Decimal → Octal
         int octal = 0;
         base = 1;
         while(decimal > 0){
